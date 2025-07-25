bd325b1587d87b6f02932b66fa46a49dc74a8edd
Manoj Karekar <96691353+manojkarekar@users.noreply.github.com>
2025-07-25 12:15:32 +0530

    Delete Django Project/ProductTest directory

--- Django Project/ProductTest/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductListCreateAPIView  , add_product_page
-
-urlpatterns = [
-   
-    path('products/', ProductListCreateAPIView.as_view(), name='product-list-create'),
-]
